97a6630792f9bf32f1df6e9677113b302d06ec74
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2018-04-04 18:09:54 +0300

    Updated deploy commands

--- deploy.sh
+++ deploy.sh
@@ -14,3 +14,9 @@ php artisan migrate --force
 
 # Change assets version to break browser cache
 php artisan assets:version
+
+# Cache some stuff
+php artisan route:cache
+
+# Destroy application cache to allow for structural changes
+php artisan cache:clear
