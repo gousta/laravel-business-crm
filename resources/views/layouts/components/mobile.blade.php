fb5af9bde2db3444886b9a628ec70b36b5632991
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2023-10-20 10:22:25 +0300

    Changed mobile theme color

--- resources/views/layouts/components/mobile.blade.php
+++ resources/views/layouts/components/mobile.blade.php
@@ -5,7 +5,7 @@
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
-    <meta name="theme-color" content="#E91E63" />
+    <meta name="theme-color" content="#32c787" />
     <link rel="manifest" href="/manifest.json">
     <link rel="apple-touch-icon" href="/web_hi_res_512.png">
     <link rel="apple-touch-icon" sizes="76x76" href="/web_hi_res_512.png">
