3dcef97a10a6f48c373180913927f3ae8ad054cc
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2023-10-20 12:01:28 +0300

    Added calendar selector

--- resources/views/appointment/index.blade.php
+++ resources/views/appointment/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app', ['pageTitle' => 'Ραντεβού' ])
 
+@push('styles')
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+@endpush
+
 @push('headercenter')
     <li>
         <ul class="hi-menu">
@@ -7,7 +11,8 @@
                 <a href="{{ $date_prev_link }}" class="action-create"><i class="him-icon zmdi zmdi-arrow-left"></i></a>
             </li>
             <li class="hi-logo">
-                <a href="#">{{$date_formatted}}</a>
+                <a href="#" class="date-selector">{{$date_formatted}}</a>
+                <input class="date-input" style="display:none" />
             </li>
             <li>
                 <a href="{{ $date_next_link }}" class="action-create"><i class="him-icon zmdi zmdi-arrow-right"></i></a>
@@ -26,5 +31,23 @@
 @stop
 
 @push('scripts')
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
 
+            var calendar = $('.date-input').flatpickr({
+                enableTime: false,
+                nextArrow: '<i class="zmdi zmdi-long-arrow-right" />',
+                prevArrow: '<i class="zmdi zmdi-long-arrow-left" />',
+                onChange: (datetime, date) => {
+                    window.location.href = "{{ route('appointment.index', ['date'=>'_date']) }}".replace('_date', date);
+                }
+            });
+            $(document).on('click', '.date-selector', function (e) {
+                e.preventDefault();
+
+                calendar.open();
+            });
+        });
+    </script>
 @endpush
