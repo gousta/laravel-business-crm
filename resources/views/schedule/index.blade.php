3a6f847b1b17a6037c6965273c0ac7ce461e2302
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2023-11-02 21:27:59 +0200

    Fixes in styling for top bar on schedule

--- resources/views/schedule/index.blade.php
+++ resources/views/schedule/index.blade.php
@@ -6,28 +6,21 @@
 
 @push('styles')
     <style>
-        .date-selector {
-            display: block !important;
-        }
-
-        .date-selector-short {
-            display: none !important;
-        }
 
         .date-input {
             display:none;
             visibility:hidden;
         }
 
-        .hi-menu > li.arrow-button.disabled:hover > a {
-            background: transparent;
+        .disabled:hover > a {
+            background: transparent !important;
         }
 
-        .hi-menu > li.arrow-button.disabled > a > .him-icon  {
+        .disabled > a > .him-icon  {
             color: #25ac72;
         }
 
-        .hi-menu > li.arrow-button.disabled a:hover  {
+        .disabled a:hover  {
             cursor: default;
         }
 
@@ -36,21 +29,9 @@
                 float: none;
             }
 
-            .date-selector {
-                display: none !important;
-            }
-
-            .date-selector-short {
-                display: block !important;
-            }
-
             .hi-menu > li.arrow-button  {
                 min-width: auto;
                 width: 35px;
-            }
-
-            #header .hi-logo a.date-selector {
-                font-size: 16px;
             }
         }
     </style>
@@ -63,8 +44,8 @@
             <li class="arrow-button {{$date_prev_link ? '':'disabled'}}">
                 <a href="{{$date_prev_link ? $date_prev_link:'javascript:;'}}" {{$date_prev_link ? '':'disabled'}}><i class="him-icon zmdi zmdi-arrow-left"></i></a>
             </li>
-            <li class="hi-logo">
-                <a href="#" class="date-selector">{{$date_week_formatted}}</a>
+            <li class="hi-logo disabled">
+                <a href="javascript:;" class="date-selector">{{$date_week_formatted}}</a>
             </li>
             <li class="arrow-button">
                 <a href="{{ $date_next_link }}"><i class="him-icon zmdi zmdi-arrow-right"></i></a>
